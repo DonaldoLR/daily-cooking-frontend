4140492164a30449ee9ad8175aeb9c98489fb460
github username <donaldolr0206@gmail.com>
2021-10-01 12:47:44 -0400

    added bootsrap lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navigation from "./components/Navigation";
+
 function App() {
 	const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-	return <div className='App'>{BASE_URL}</div>;
+	return (
+		<div className='App'>
+			<Navigation />
+			{BASE_URL}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Navigation = () => {
+	return <div>Hi!</div>;
+};
+
+export default Navigation;
