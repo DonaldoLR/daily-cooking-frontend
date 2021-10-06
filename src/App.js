3a752236785cce713b6cf07b92936ce699abc64c
github username <donaldolr0206@gmail.com>
2021-10-06 10:26:46 -0400

    completed Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import RecipesContainer from "./components/RecipesContainer";
 
 function App() {
-	const BASE_URL = process.env.REACT_APP_BASE_URL;
+	// const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 	return (
-		<div className='App'>
-			<Navigation />
-			{BASE_URL}
-		</div>
+		<Router>
+			<div className='App'>
+				<Navigation />
+				<Switch>
+					<Route exact path='/recipes'>
+						<RecipesContainer />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	NavLink,
+} from "react-router-dom";
 
 const Navigation = () => {
 	return (
-		<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+		<nav className='navbar sticky-top navbar-expand-lg navbar-dark bg-dark'>
 			<div className='container-fluid'>
-				<a className='navbar-brand' href='#'>
+				<NavLink className='navbar-brand' to='#'>
 					Daily Cooking
-				</a>
+				</NavLink>
 				<button
 					className='navbar-toggler'
 					type='button'
@@ -20,65 +26,22 @@ const Navigation = () => {
 					<span className='navbar-toggler-icon'></span>
 				</button>
 				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+					<ul className='navbar-nav me-auto mb-2 mb-lg-0 '>
 						<li className='nav-item'>
-							<a className='nav-link active' aria-current='page' href='#'>
+							<NavLink className='nav-link active' aria-current='page' to='/'>
 								Home
-							</a>
+							</NavLink>
 						</li>
 						<li className='nav-item'>
-							<a className='nav-link' href='#'>
+							<NavLink className='nav-link' to='/recipes'>
 								Recipes
-							</a>
-						</li>
-						<li className='nav-item dropdown'>
-							<a
-								className='nav-link dropdown-toggle'
-								href='#'
-								id='navbarDropdown'
-								role='button'
-								data-bs-toggle='dropdown'
-								aria-expanded='false'
-							>
-								Contact Us
-							</a>
-							<ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
-								<li>
-									<a className='dropdown-item' href='#'>
-										Action
-									</a>
-								</li>
-								<li>
-									<a className='dropdown-item' href='#'>
-										Another action
-									</a>
-								</li>
-								<li>
-									<hr className='dropdown-divider' />
-								</li>
-								<li>
-									<a className='dropdown-item' href='#'>
-										Something else here
-									</a>
-								</li>
-							</ul>
-						</li>
-						<li className='nav-item'>
-							<a className='nav-link disabled'>Disabled</a>
+							</NavLink>
 						</li>
 					</ul>
-					<form className='d-flex'>
-						<input
-							className='form-control me-2'
-							type='search'
-							placeholder='Search'
-							aria-label='Search'
-						/>
-						<button className='btn btn-outline-success' type='submit'>
-							Search
-						</button>
-					</form>
 				</div>
+				<NavLink className='btn btn-outline-success' to='/newRecipe'>
+					Create Recipe
+				</NavLink>
 			</div>
 		</nav>
 	);
