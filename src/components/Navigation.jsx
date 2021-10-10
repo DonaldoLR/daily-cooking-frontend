0314a3fcee42d9df6e6984b274a18d25d2903963
github username <donaldolr0206@gmail.com>
2021-10-10 15:54:30 -0400

    mobile navigation is complete

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,60 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
+	const [menuToggle, setMenuToggle] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuToggle(!menuToggle);
+	};
 	return (
 		<nav>
 			<div className='container'>
 				<div className='brand'>
-					<p>Culinier</p>
+					<NavLink to='/'>Culinier</NavLink>
 				</div>
-				<button class='hamburger' type='button'>
-					<span class='hamburger-box'>
-						<span class='hamburger-inner'></span>
+				<button
+					className={
+						'hamburger hamburger--spin ' + (menuToggle ? 'is-active' : '')
+					}
+					type='button'
+					onClick={toggleMenu}>
+					<span className='hamburger-box'>
+						<span className='hamburger-inner'></span>
 					</span>
 				</button>
+				<ul className={menuToggle && 'open'}>
+					<li>
+						<NavLink to='/' onClick={toggleMenu}>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/recipes' onClick={toggleMenu}>
+							Recipes
+						</NavLink>
+					</li>
+					{/* <li>
+						<NavLink to='/blog' onClick={toggleMenu}>Blog</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallary' onClick={toggleMenu}>Gallary</NavLink>
+					</li>
+					<li>
+						<NavLink to='/about' onClick={toggleMenu}>About</NavLink>
+					</li>
+					<li>
+						<NavLink to='/contact' onClick={toggleMenu}>Contact</NavLink>
+					</li>
+					<li>
+						<NavLink to='/login' onClick={toggleMenu}>Login</NavLink>
+					</li> */}
+					<li>
+						<NavLink to='/newRecipe' onClick={toggleMenu}>
+							Create Recipe
+						</NavLink>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
